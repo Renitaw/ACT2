fcf92f32b0a2204741ea0b4d5170e015da998b7e
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-10-20 17:12:48 -0700

    Memasukkan Array bilangan untuk menahan nilai

--- ACT2/Program.cs
+++ ACT2/Program.cs
@@ -6,5 +6,10 @@ using System.Threading.Tasks;
 
 namespace Quicksort
 {
-
+    class program
+    {
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+    }
 }
